82775362f19d9b2676907c8e80c9714a3a1bba8b
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-09-20 22:34:20 +0500

    first commit

---
+++ script.ts
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    
+    const allToggleButtons = document.querySelectorAll('.toggle-button');
+    
+    allToggleButtons.forEach(element => {
+        element.addEventListener('click',()=>{
+
+         const togglebutton = element.getAttribute('data-target');
+
+         if(togglebutton && togglebutton.trim()){
+             const targetElement = document.getElementById(togglebutton);
+        if(targetElement){
+             if(targetElement.style.display === 'block' || targetElement.style.display === ' '){
+                targetElement.style.display = 'none';
+
+             }else{
+                 targetElement.style.display = 'block';
+             }
+             
+         }else{
+             console.log(`No target element found ${togglebutton}`);
+         }
+        }else{
+            console.log('No data-target attribute found');
+ 
+        }
+         
+        })
+        
+    });
+});
